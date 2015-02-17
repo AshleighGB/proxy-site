71dbd0ec18cff62ea1b66262a10aa82ce7ad7d8c
SystemsAdministrator <joshuawareing@outlook.com>
2015-02-17 10:12:02 +0000

    Added All Proxy files

---
+++ pages/error.php
@@ -0,0 +1,4 @@
+<?php $ip = $_SERVER['REMOTE_ADDR']; print 'ERROR: Your IP: '.$ip; Does not have access to this page because a HTACCESS file is preventing it.
+//echo 'Server encountered an error. sorry.';
+
+?>
